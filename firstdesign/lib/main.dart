97c489f399e5881fab4e53b0167d6e7a047e6dfd
SSalahAyman <salahayman488@gmail.com>
2022-10-16 20:33:16 +0200

    its done

---
+++ firstdesign/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Design1());
+}
+class Design1 extends StatelessWidget {
+  Design1();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Color(0xFF4E7182),
+            appBar: AppBar(
+              title: Text("I Am Rich"),
+              centerTitle: true,
+              backgroundColor: Color(0XFF202B31),
+            ),
+            body:Center(
+              child: Image(image:AssetImage("assets/diamond.png"),
+                width: 450,
+                height:450,
+                fit: BoxFit.fitHeight,
+              ),
+            )
+        )
+    );
+  }
+}
